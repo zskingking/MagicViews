bc4893644e1eaa4f4152a186c8e3de3f3d0267a7
zs.bezier <495968956@qq.com>
2020-07-23 18:15:23 +0800

    新建tagLayout

---
+++ tag/src/main/java/com/zs/tag/TagLayout.kt
@@ -0,0 +1,18 @@
+package com.zs.tag
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+/**
+ * des 根据多个tag进行平铺换行
+ * @date 2020/7/23
+ * @author zs
+ */
+class TagLayout(context: Context?, attrs: AttributeSet?) : ViewGroup(context, attrs) {
+
+
+
+    override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
+    }
+}
